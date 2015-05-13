ab336e889a262026cf44d94a082afed2dfa64f77
Andrew Vandeyar <warpstar360100@yahoo.com>
2015-05-13 15:13:51 -0700

    added simple floor, follow camera, player, and AI opponent. AI follows player.

---
+++ ProjectElimination/Assets/Scripts/BasicAI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(AICharacterControl))]
+public class BasicAI : MonoBehaviour {
+
+	private AICharacterControl pAICC;
+
+	// Use this for initialization
+	void Start () {
+		pAICC = GetComponent<AICharacterControl> ();
+	}
+
+}
---
+++ ProjectElimination/Assets/Scripts/CharacterActions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterActions : MonoBehaviour {
+
+	private Rigidbody pRigidBody;
+	private Queue<Actions> pActionQueue = new Queue<Actions> ();
+
+	public delegate void ActionDelegate();
+	public float ShoveForce = 100f;
+	public class Actions {
+		public ActionDelegate myAction;
+	
+		// Pushes 
+//		public void Shove() {
+//			pRigidBody.AddForce (transform.forward * ShoveForce, ForceMode.Impulse);
+//		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		pRigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		Actions[] actionList = new Actions[pActionQueue.Count];
+		pActionQueue.CopyTo (actionList, 0);
+		pActionQueue.Clear ();
+		foreach (Actions act in actionList) {
+			act.myAction();
+		}
+	}
+
+	public void Shove() {
+		pRigidBody.AddForce (transform.forward * ShoveForce, ForceMode.Impulse);
+	}
+
+	//Queues action
+	public void PerformAction(Actions act) {
+		pActionQueue.Enqueue (act);
+	}
+}
