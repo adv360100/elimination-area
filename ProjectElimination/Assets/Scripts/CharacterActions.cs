0b7078dd0aa40d739b872599831fbfe7a07d6d7f
Andrew Vandeyar <warpstar360100@yahoo.com>
2015-05-19 12:04:31 -0700

    can knockback the AI

--- ProjectElimination/Assets/Scripts/CharacterActions.cs
+++ ProjectElimination/Assets/Scripts/CharacterActions.cs
@@ -5,23 +5,51 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody))]
 public class CharacterActions : MonoBehaviour {
 
-	private Rigidbody pRigidBody;
+	//private Rigidbody pRigidBody;
 	private Queue<Actions> pActionQueue = new Queue<Actions> ();
 
-	public delegate void ActionDelegate();
 	public float ShoveForce = 100f;
-	public class Actions {
-		public ActionDelegate myAction;
-	
-		// Pushes 
-//		public void Shove() {
-//			pRigidBody.AddForce (transform.forward * ShoveForce, ForceMode.Impulse);
-//		}
+	public Collider Hitbox;
+	public bool IsPlayerControlled = true;
+
+	public abstract class Actions {	
+		// performs action 
+		public abstract void PerformAction ();
 	}
+	public class ShoveAction : Actions {
+		public Vector3 shoveDir;
+		public float shovePower;
+
+		private Rigidbody targetBody;
+
+		// constructor that takes in the target of the shove action
+		public ShoveAction(Rigidbody target) {
+			targetBody = target;
+		}
+
+		public override void PerformAction() {
+			targetBody.AddForce (shoveDir * shovePower, ForceMode.Impulse);
+		}
+	}
+
 
 	// Use this for initialization
 	void Start () {
-		pRigidBody = GetComponent<Rigidbody> ();
+		//pRigidBody = GetComponent<Rigidbody> ();
+		Hitbox.gameObject.SetActive(false);
+	}
+
+	void Update() {
+		if (IsPlayerControlled == false)
+			return;
+
+		if (Input.GetButtonDown ("Fire1")) {
+			// todo: animate
+
+			Hitbox.gameObject.SetActive(true);
+		} else {
+			Hitbox.gameObject.SetActive(false);
+		}
 	}
 	
 	void FixedUpdate () {
@@ -29,16 +57,25 @@ public class CharacterActions : MonoBehaviour {
 		pActionQueue.CopyTo (actionList, 0);
 		pActionQueue.Clear ();
 		foreach (Actions act in actionList) {
-			act.myAction();
+			act.PerformAction();
 		}
 	}
 
-	public void Shove() {
-		pRigidBody.AddForce (transform.forward * ShoveForce, ForceMode.Impulse);
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player" || other.tag == "Enemy") {
+			Rigidbody target = other.GetComponent<Rigidbody> ();
+			Vector3 dir = other.transform.position - Hitbox.transform.position;
+			dir.y = 0f;
+			dir.Normalize();
+			ShoveTarget (target, dir, ShoveForce);
+		}
 	}
 
 	//Queues action
-	public void PerformAction(Actions act) {
+	public void ShoveTarget(Rigidbody target, Vector3 shoveDir, float power) {
+		ShoveAction act = new ShoveAction (target);
+		act.shoveDir = shoveDir;
+		act.shovePower = power;
 		pActionQueue.Enqueue (act);
 	}
 }
